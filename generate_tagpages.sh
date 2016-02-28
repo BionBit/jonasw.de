e97fe40a1f26456f715fbee5f139f63cbf0c4b41
Jonas Weber <github@jonasw.de>
2016-02-28 12:57:34 +0100

    Tag pages

---
+++ generate_tagpages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+TAGS=$(grep -hr categor _posts | cut -d ":" -f2 | tr -d '[],' | tr ' ' '\n' | grep -e "^." | sort | uniq)
+for tag in $TAGS; do
+
+	mkdir -p ./tags/$tag/
+	cat > ./tags/$tag/index.md <<EOFX
+---
+layout: tagpage
+tag: $tag
+---
+
+EOFX
+done
